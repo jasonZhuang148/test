856785cd0d08e93b8b7f2d5a4c98a4f65768b4a9
jason.zhuang <jason.zhuang@lafayette148.com>
2018-12-01 15:30:30 +0800

    first commit

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 
 console.log('Script loaded!')
-var cacheStorageKey = 'minimal-pwa-82'
+var cacheStorageKey = 'minimal-pwa-83'
 
 var cacheList = [
   '/',
   "index.html",
   "main.css",
   "e.png",
-  "512.png",
-  "192.png",
+  "assets/icons/icon-48x48.png",
+  "assets/icons/icon-72x72.png",
+  "assets/icons/icon-96x96.png",
+  "assets/icons/icon-128x128.png",
+  "assets/icons/icon-144x144.png",
+  "assets/icons/icon-152x152.png",
+  "assets/icons/icon-192x192.png",
+  "assets/icons/icon-512x512.png",
   "pwa-fonts.png"
 ]
 
